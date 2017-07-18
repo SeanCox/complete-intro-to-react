751d9fc36beb9f53f9905ff12f4cf8fae600b405
Sean Cox <sean@marle.attlocal.net>
2017-07-17 20:32:53 -0400

    scox/v3-03

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,22 +1,14 @@
 import React from 'react'
 import { render } from 'react-dom'
 
-const ce = React.createElement
+const App = () => (
+  <div className="app">
+    <div className="landing">
+      <h1>svideo</h1>
+      <input type="text" placeholder="Search" />
+      <a>or Browse All</a>
+    </div>
+  </div>
+)
 
-const MyTitle = function(props) {
-  return ce('div', null, ce('h1', { style: { color: props.color } }, props.title))
-}
-
-const MyFirstComponent = function() {
-  return ce(
-    'div',
-    { id: 'my-first-component' },
-    ce(MyTitle, { title: 'Game of Thrones', color: 'cadetblue' }),
-    ce(MyTitle, { title: 'Sillicon Valley', color: 'peru' }),
-    ce(MyTitle, { title: 'Archer', color: 'lime' }),
-    ce(MyTitle, { title: 'The Last Airbender', color: 'slateblue' }),
-    ce(MyTitle, { title: 'Firefly', color: 'firebrick' })
-  )
-}
-
-render(ce(MyFirstComponent), document.getElementById('app'))
+render(<App />, document.getElementById('app'))
